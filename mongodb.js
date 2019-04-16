32453b5b6d1d7987acd4693e889701fdfb702870
zarab <musfiqzarab@iut-dhaka.edu>
2019-04-16 15:25:23 +0600

    Inserting documents

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 //CRUD operation
 
-const mongodb = require("mongodb");
+const mongodb = require("mongodb"); //mongodb is a native driver
 const MongoClient = mongodb.MongoClient; // let us connect to database
 
 const connectionURL = "mongodb://127.0.0.1:27017"; //connect to localhost server
@@ -15,9 +15,64 @@ MongoClient.connect(
     }
     // console.log("Connection Established");
     const db = client.db(databaseName); // autometicaly creates the database if not found
-    db.collection("users").insertOne({
-      name: "Zarab",
-      age: 25
-    });
+
+    //insert one document
+    db.collection("users").insertOne(
+      {
+        name: "Zarab",
+        age: 25
+      },
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert user");
+        }
+
+        console.log(result.ops);
+      }
+    );
+
+    //insert multiple documents
+    db.collection("users").insertMany(
+      [
+        {
+          name: "Mou",
+          age: 24
+        },
+        {
+          name: "Raju",
+          age: 21
+        }
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert documents");
+        }
+        console.log(result.ops);
+      }
+    );
+
+    //insert multiple documents
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "Eating lunch",
+          completed: true
+        },
+        {
+          description: "Performing salah",
+          completed: false
+        },
+        {
+          description: "Charging my phone",
+          completed: true
+        }
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert documents");
+        }
+        console.log(result.ops);
+      }
+    );
   }
 );
