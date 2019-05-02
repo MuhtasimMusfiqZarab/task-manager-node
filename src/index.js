22e3aa24f99625b5539734f24bde10f8d9cf3ddf
zarab <musfiqzarab@iut-dhaka.edu>
2019-05-02 14:36:00 +0600

    Using Express Middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,30 @@ const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+//register new middleware func
+//works between the request coming to server & running route handler                         with middleware: new request --> middleware does something--> run route handler
+//next is what is specific to middleware
+// app.use((req, res, next) => {
+//   console.log(req.method);
+//   if (req.method === "GET") {
+//     res.send("Get methods are disabled");
+//   } else {
+//     next();
+//   }
+//   //if next is not called then the request gets stuck to this middleware function and does not run route handler
+//   // next();
+// });
+
+//registering middleware for maintainance mode
+app.use((req, res, next) => {
+  if (req) {
+    res.send("The Server is under maintainance");
+  } else {
+    next();
+  }
+});
+
 //parse incoming postman JSON(which is a request) for us to get accesible as an object we can use in req.body
 app.use(express.json());
 //registering the router to express
